acdf04c78832d0ce7c86c99913489efe438e90b9
Jesus Bejar <israelbejarii@gmail.com>
2023-01-05 15:35:08 -0700

    Added some documentation to activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
+        Console.Write("What is your first name? ");
+        // Write is different from WriteLine
+        // Write line outputs beside the input message 
+        // Writeline outputs underneath the input message
         string first_name = Console.ReadLine();
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
