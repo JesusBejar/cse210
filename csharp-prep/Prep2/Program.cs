2df273bd89d682b05702ec07cb15f75b1b33a328
Jesus Bejar <israelbejarii@gmail.com>
2023-01-07 19:28:44 -0700

    Finished C# Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string userGrade = Console.ReadLine();
+
+        int x = int.Parse(userGrade);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = ("A");
+
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else if (x >= 80)
+        {
+            letter = ("B");
+
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else if (x >= 70)
+        {
+            letter = ("C");
+
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else if (x >= 60)
+        {
+            letter = ("D");
+
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else 
+        {
+            letter = ("F");
+
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        if (x >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("Study so you can pass the class with a grade of 70 or more");
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
     }
 }
