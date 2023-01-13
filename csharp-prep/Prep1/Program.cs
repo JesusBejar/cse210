fdb14b99fa051b883ee3c8c0cb0d7f89f9aacfaf
Jesus Bejar <israelbejarii@gmail.com>
2023-01-13 16:37:04 -0700

    Finished Prep 4 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("What is your first name? ");
         // Write is different from WriteLine
-        // Write line outputs beside the input message 
+        // Write outputs beside the input message 
         // Writeline outputs underneath the input message
         string first_name = Console.ReadLine();
         Console.Write("What is your last name? ");
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List <int> numbers = new List<int> ();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int userNums = -1;
+
+        while (userNums != 0) {
+            Console.Write("Enter a number!: ");
+            string userStrs = Console.ReadLine();
+
+            userNums = int.Parse(userStrs);
+
+            // add parsed, int numbers to numbers list
+            numbers.Add(userNums);
+        }
+
+        // used to make sure userNums was being added to List
+        // foreach (int number in numbers) {
+        //     Console.WriteLine($"{number}");
+        // }
+
+        // this is the Sum function 
+        // data type variableName = listName.Sum();
+        int sum = numbers.Sum();
+        Console.WriteLine($"This is the sum {sum} ");
+
+        // this is called the Enumerable.Average method
+        // double average = listName.Average();
+        double average = numbers.Average();
+        Console.WriteLine($"This is the average {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers) 
+        {
+            if ( number > max)
+            {
+                max = number;
+            }
+
+            
+        }
+
+        Console.WriteLine($"This is the max {max}");
     }
 }
