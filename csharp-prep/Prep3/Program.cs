d45fa02323b60d4c5ecd9c6d2409975ea228dcbe
Jesus Bejar <israelbejarii@gmail.com>
2023-01-13 13:39:48 -0700

    Finished Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1 ,100);
+
+        // Console.Write($"This is your {magicNum} ");
+        // string magicNum = "";
+
+        int userNum = -1;
+
+        while (userNum != magicNum){
+
+            Console.Write("What is your guess? ");
+            string userStr = Console.ReadLine();
+
+            // do not need to define data type if it has 
+            // defined already
+            // ex. int userNum = int.Parse(userStr);
+            userNum = int.Parse(userStr);
+
+            if (userNum > magicNum) {
+                Console.WriteLine("Guess Lower");
+            }
+
+            else if (userNum < magicNum) {
+                Console.WriteLine("Guess Higher");
+            }
+         }
+    
     }
 }
+        
