bfa256e18ace907a4e985cd6425ff3a88cac6678
Jesus Bejar <israelbejarii@gmail.com>
2023-01-13 17:21:23 -0700

    Finished Prep assignment 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // this is f1 CHECK
+        static void DisplayWelcome() 
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        // call 1
+        DisplayWelcome();
+
+        // this is f2 CHECK
+        static string PromptUserName() 
+        {
+            Console.Write("Enter your name: ");
+            string userNameInput = Console.ReadLine();
+            return userNameInput;
+        }
+        string userName = PromptUserName();
+
+        // this is f3
+        static int PromptUserNumber() 
+        {
+            Console.Write("Enter your favorite number: ");
+            string favNumberInput = Console.ReadLine();
+            int favNum = int.Parse(favNumberInput);
+            // below is a way to combine the two lines above
+            // int favNum = int.Parse(Console.ReadLine());
+            return favNum;
+        }
+        int userNum = PromptUserNumber();
+
+        // this is f4
+        static int SquareNumber(int number) 
+        {
+            int squaredNum = (number * number);
+            return squaredNum;
+        }
+        int squaredNum = SquareNumber(userNum);
+
+        // this is f5
+        static void DisplayResult(string userName, int squaredNumber) 
+        {
+            Console.WriteLine ($"{userName}, your number squared {squaredNumber} ");
+        }
+        // call 5
+        DisplayResult(userName, squaredNum);
+        
     }
 }
