6d0c809b618217b0cc47b90658827d77c8d034ff
Jesus Bejar <israelbejarii@gmail.com>
2023-02-01 09:22:41 -0700

    finished Learning Activity 3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Fraction
+{
+   private int _top;
+   private int _bottom;
+
+   public Fraction () {
+       _top = 1;
+       _bottom = 1;
+   }
+   public Fraction (int wholeNum) {
+       _top = wholeNum;
+       _bottom = 1;
+   }
+   public Fraction (int top, int bottom) {
+       _top = top;
+       _bottom = bottom;
+   }
+    // Create a method called GetFractionString 
+    // that returns the fraction in the form 3/4.
+   public string GetFractionString () {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    // Create a method called GetDecimalValue that returns a double that is the result of 
+    // dividing the top number by the bottom number, such as 0.75.
+    public double GetDecimalValue () {
+        return (double)_top / (double)_bottom;
+    }
+
+}
