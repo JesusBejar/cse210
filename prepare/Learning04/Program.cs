f05852f374011adb2b480f5348dfcb6e5e581976
Jesus Bejar <israelbejarii@gmail.com>
2023-02-16 10:35:14 -0700

    Finished writing the WritingAssignment class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        // 1
         BaseAssignment _simpleAssignment = new BaseAssignment("Monk", "Calculus");
         
         // assign the method call to a variable then call the variable
         string summary = _simpleAssignment.GetSummary();  
 
         Console.WriteLine(summary); 
+
+        // 2
+        MathAssignment _newAssignment = new MathAssignment("Mulder", "History", "Section 10.1", "Problems 10-15");
+
+        string summary2 = _newAssignment.GetSummary();  
+
+        Console.WriteLine(summary2); 
+
+        string _sectionAndProblems = _newAssignment.GetHomeworkList();
+
+        Console.WriteLine(_sectionAndProblems);
+
+        // 3
+        WritingAssignment _writingAssignment = new WritingAssignment("Scully", "Medicine", "The Truth");
+
+        string summary3 = _writingAssignment.GetSummary();  
+
+        Console.WriteLine(summary3); 
+
+        string _titleName = _writingAssignment.GetWritingInfo();
+
+        Console.WriteLine(_titleName);
     }
 }
