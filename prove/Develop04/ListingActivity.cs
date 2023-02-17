b2962b6e3ed0d5af130502776135d9dbcaeaf867
Jesus Bejar <israelbejarii@gmail.com>
2023-02-17 10:06:04 -0700

    Wrote code to display opening msg and prompt

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,4 +1,7 @@
-public class ListingAssignment
+public class ListingActivity : BaseActivity
 {
-    
+    public ListingActivity(string openingPrompt, string startingMessage) : base (openingPrompt, startingMessage) 
+    {
+
+    }
 }
---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,7 @@
+public class ReflectingActivity : BaseActivity
+{
+    public ReflectingActivity(string openingPrompt, string startingMessage) : base (openingPrompt, startingMessage) 
+    {
+
+    }
+}
