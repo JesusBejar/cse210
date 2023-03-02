c196c397c2adf0231fbfc21603899ac7980aa8f7
Jesus Bejar <israelbejarii@gmail.com>
2023-03-02 10:26:06 -0700

    added list for shapes and loop for display

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,12 +6,21 @@ class Program
     {
         Console.WriteLine("Hello Learning05 World!");
 
+        List<Shape> shapes = new List<Shape>();
+
         Square square = new Square("blue", 2);
-        Console.WriteLine(square.GetColor());
-        Console.WriteLine(square.GetArea());
+        shapes.Add(square);
 
         Rectangle rectangle = new Rectangle("red", 3, 2);
-        Console.WriteLine(rectangle.GetColor());
-        Console.WriteLine(rectangle.GetArea());
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("purple", 3);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes )
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
