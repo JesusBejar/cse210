16aaef4662bc188aff283a61e5b81607069cd9a3
Jesus Bejar <israelbejarii@gmail.com>
2023-03-06 09:37:28 -0700

    populated program with menu & conditionals

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int _userMenuChoice ;
+
+        Console.WriteLine("Menu Options");
+        Console.WriteLine("1.Create New Goal");
+        Console.WriteLine("2.List Goals");
+        Console.WriteLine("3.Save Goals");
+        Console.WriteLine("4.Load Goals");
+        Console.WriteLine("5.Record Event");
+        Console.WriteLine("6.Quit");
+        Console.WriteLine("Please select a choice from the menu: ");
+        _userMenuChoice = Int32.Parse(Console.ReadLine());
+
+        if (_userMenuChoice == 1) {
+            
+        }
+        
+        else if (_userMenuChoice == 2) {
+           
+        }
+
+        else if (_userMenuChoice == 3) {
+            
+        }
+
+        else if (_userMenuChoice == 4) {
+            
+        }
+
+        else if (_userMenuChoice == 5) {
+            
+        }
+        
+        else 
+        {
+            System.Environment.Exit(0); 
+        }
     }
 }
