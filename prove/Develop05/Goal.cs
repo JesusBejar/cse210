66486cbe4aab1cd40ba0203afeea962b381f2965
Jesus Bejar <israelbejarii@gmail.com>
2023-03-06 08:54:53 -0700

    added attributes to base class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,4 +1,14 @@
 class Goal 
 {
+    protected int _userMenuChoice ;
+    protected string _goalName ;
+    protected string _goalDescription ;
+    protected int _goalPoints ;
+    protected int _bonusPoints ;
+    protected int _counter ;
+    protected List<string> _goals ;
+
+
     
+
 }
