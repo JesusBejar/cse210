76eafe5fff37a136241b40c2c66ad68034478b25
Jesus Bejar <israelbejarii@gmail.com>
2023-03-11 21:58:59 -0700

    finished writing code for Load Goal class

--- prove/Develop05/LoadGoal.cs
+++ prove/Develop05/LoadGoal.cs
@@ -1,7 +1,34 @@
 class LoadGoal : BaseGoal 
 {
+    public LoadGoal(List<string> _goals) : base ( _goals)
+    {
+        
+    }
     public override string SpecificGoalTask()
     {
-        return "1";
+        Console.WriteLine("Please enter the name of your file:");
+        string filename = Console.ReadLine();
+        try  
+        {
+        // create a StreamReader similar to streamwriter
+            using (StreamReader reader = new StreamReader(filename))  
+            {  
+                string line;  
+                // read file line by line with while loop
+                while ((line = reader.ReadLine()) != null)  
+                    {  
+                        _goals.Add(line);
+                        // foreach (string g in _goals) 
+                        //     {
+                        //         (_goals.IndexOf(g)+1);
+                        //     }
+                    }  
+            }
+        }  
+        catch (Exception exp)  
+        {  
+            Console.WriteLine(exp.Message);  
+        }  
+        return "";
     }
 }
