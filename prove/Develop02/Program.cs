afbf1fbc2e9e17aaa49f27228dc3a5fba0f2ff27
Jesus Bejar <israelbejarii@gmail.com>
2023-03-16 08:42:43 -0600

    developed random prompt generator

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,18 +1,18 @@
 using System;
 
-public class main
+public class Program
 {
-
     static void Main(string[] args)
     {
-    
         // int randomNum = random.Next(0, _promptReference._questionPrompts.Count);
         // _promptReference.randomNum = randomNum;
-
-        //     _date = DateTime.Now.ToString("dddd, dd MMMM yyyy");
-
             {
-            
+            string _userAnswer = null;
+
+            string _displayedPrompt = null;
+
+        while (_userAnswer != "5")
+        {
             Console.WriteLine ("Please select one of the following choices");
             Console.WriteLine ("1) Write");
             Console.WriteLine ("2) Display");
@@ -22,19 +22,35 @@ public class main
             Console.WriteLine("What would you like to do? ");
             int _userChoice = int.Parse(Console.ReadLine());
 
+
             {
             if (_userChoice == 1)
             {
+                PromptGenerator rp = new PromptGenerator();
+                // populate prompt list
+                rp.PopulatePromptList();
+                // randomly generated prompt
+                _displayedPrompt = rp.DisplayRandomPrompt();
+                // user response
+                Console.Write(">");
+                _userAnswer = Console.ReadLine();
 
             }
 
             else if (_userChoice == 2)
             {
+                Entry e = new Entry();
+                DateTime todaysDate = DateTime.Today;
 
+                e._date = todaysDate.ToString();
+                e._userAnswer = _userAnswer;
+                e._ranPrompt = _displayedPrompt;
+
+                e.DisplayEntry();
             }
             else if (_userChoice == 3)
             {
-
+                
             }
             else if (_userChoice == 4)
             {
@@ -44,6 +60,8 @@ public class main
             {
                 System.Environment.Exit(0); 
             }
+        }
+            
         }
 
     }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,15 +1,22 @@
 public class PromptGenerator
 {   
-    // public string _journalPrompt ;
-
-    public string _currentDate;
-
-    public int randomNum;
-
-    public List<string> _questionPrompts = new List<string>();
-    public void DisplayPrompt() 
+    public string _ranPrompt ;
+    public int _ranNum ;
+    public List<string> _promptList = new List<string>();
+    public void PopulatePromptList()
     {
-        Console.WriteLine(_questionPrompts[randomNum]);
+        _promptList.Add("Who was the most interesting person I interacted with today?");
+        _promptList.Add("What was the best part of my day?");
+        _promptList.Add("How did I see the hand of the Lord in my life today?");
+        _promptList.Add("What was the strongest emotion I felt today?");
+        _promptList.Add("If I had one thing I could do over today, what would it be?");
     }
-
+    public string DisplayRandomPrompt() 
+    {
+        Random random = new Random();
+        _ranNum = random.Next(_promptList.Count());
+        _ranPrompt = _promptList[_ranNum];
+        Console.WriteLine($"{_ranPrompt}");
+        return $"{_ranPrompt}";
+    }
 }
