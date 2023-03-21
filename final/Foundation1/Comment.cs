90da2183110587d352400eced73363150715f6d2
Jesus Bejar <israelbejarii@gmail.com>
2023-03-20 19:35:11 -0600

    made constructors and methods for classes

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,4 +2,14 @@ public class Comment
 {
     public string _name ;
     public string _text ;
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    public string ReturnComment()
+    {
+        return "";
+    }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -5,4 +5,20 @@ public class Video
     public int _length ;
     public List<Comment> _comments = new List<Comment>();
 
+    public Video (string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public int CommentNumberLength()
+    {
+        return 1;
+    }
+
+    public string ReturnVideo()
+    {
+        return "";
+    }
 }
