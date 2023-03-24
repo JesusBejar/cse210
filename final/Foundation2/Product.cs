986c080f8632a861ce69aa02faf10ae60c4d38c7
Jesus Bejar <israelbejarii@gmail.com>
2023-03-24 14:11:38 -0600

    completed code for Product class

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -3,11 +3,15 @@ public class Product
     private string _name ;
     private string _id ;
     private int _quantity ;
+    private int _price ;
     private int _totalPrice ;
 
-    public Product ()
+    public Product (string name, string id, int quantity, int price)
     {
-
+        _name = name;
+        _id = id;
+        _quantity = quantity;
+        _price = price;
     }
     public string ReturnProduct()
     {
@@ -16,6 +20,6 @@ public class Product
 
     public int TotalProductPrice()
     {
-        return _quantity * _totalPrice;
+        return _quantity * _price;
     }
 }
