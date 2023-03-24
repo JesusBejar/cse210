136169cd1ebf466740bbbea80d47b77dcc3747ba
Jesus Bejar <israelbejarii@gmail.com>
2023-03-24 14:12:02 -0600

    wrote test code inside of Main program

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // below prints the following
+        // 3901 Candy st Utah United States of America
+        // USA
+        Address a1 = new Address("3901 Candy st", "Utah", "United States of America");
+        Console.WriteLine(a1.ReturnAddress());
+        Console.WriteLine(a1.CountryIsUSA());
+
+        // below prints the following
+        // Elend Musk 3901 Candy st Utah United States of America
+        Customer c1 = new Customer("Elend Musk", a1);
+        Console.WriteLine(c1.ReturnCustomer());
+
+        Product bonobosPoloShirt = new Product("Soft Air Sweather Polo", "SKU: BF-104058010-UBUO", 3, 109);
+        Console.WriteLine(bonobosPoloShirt.ReturnProduct());
+        Console.WriteLine($"Total cost: {bonobosPoloShirt.TotalProductPrice()}");
     }
 }
