d52cd8c675d6ca1f8f538ff9f300e03df748941c
Jesus Bejar <israelbejarii@gmail.com>
2023-03-25 09:17:37 -0600

    added more text code

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Product> _productList = new List<Product>();
         // below prints the following
         // 3901 Candy st Utah United States of America
         // USA
@@ -19,5 +20,16 @@ class Program
         Product bonobosPoloShirt = new Product("Soft Air Sweather Polo", "SKU: BF-104058010-UBUO", 3, 109);
         Console.WriteLine(bonobosPoloShirt.ReturnProduct());
         Console.WriteLine($"Total cost: {bonobosPoloShirt.TotalProductPrice()}");
+        _productList.Add(bonobosPoloShirt);
+
+        Product championSweats = new Product("CLOSED BOTTOM EVERYDAY COTTON PANTS, 31.5", "Style# P7310-407Q88", 2, 27);
+        Console.WriteLine(championSweats.ReturnProduct());
+        Console.WriteLine($"Total cost: {championSweats.TotalProductPrice()}");
+        _productList.Add(championSweats);
+
+        CustomerOrder o1 = new CustomerOrder(_productList, c1, bonobosPoloShirt.TotalProductPrice(), a1);
+        // below will print out the name and product id of each product in the order
+        o1.PackingLabel();
+        o1.ShippingLabel();
     }
 }
