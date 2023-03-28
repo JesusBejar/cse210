8d4c0be5138e9d6e7fba5e82436119fded98995a
Jesus Bejar <israelbejarii@gmail.com>
2023-03-28 10:48:00 -0600

    consolidated all comments to one list

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         List<Video> _videoList = new List<Video>();
 
-        List<Comment> _comments = new List<Comment>();
+        List<Comment> _biggerCommentList = new List<Comment>();
 
         // VIDEO 1
         Video video1 = new Video();
@@ -16,21 +16,22 @@ class Program
         _videoList.Add(video1);
 
         // 3 COMMENTS 
+        List<Comment> _comments1 = new List<Comment>();
+        
         Comment c1 = new Comment();
         c1._name = "@DrBeauHightower";
         c1._text = "What a time to be alive. Probably the most excited me and my friends had ever been for a fight, but especially the pre fight trash talk. Iconic.";
-        _comments.Add(c1);
+        _biggerCommentList.Add(c1);
 
         Comment c2 = new Comment();
         c2._name = "@rawbebaba";
         c2._text = "I'll always love Nate Diaz man. What a treasure to the sport.";
-        _comments.Add(c2);
+        _biggerCommentList.Add(c2);
 
         Comment c3 = new Comment();
         c3._name = "@Henroin42";
         c3._text = "10:10 connors face is priceless";
-        _comments.Add(c3);
-
+        _biggerCommentList.Add(c3);
         // VIDEO 2
         Video video2 = new Video();
         video2._title = "\nHow to Add, Remove & Toggle Classes By Using DOM (classlist) property";
@@ -39,58 +40,84 @@ class Program
         _videoList.Add(video2);
 
         // 3 COMMENTS 
+        List<Comment> _comments2 = new List<Comment>();
         Comment c4 = new Comment();
         c4._name = "@lightagainst_darkness6697";
         c4._text = "Thanks man you helped me a lot";
-        _comments.Add(c4);
+        _biggerCommentList.Add(c4);
 
         Comment c5 = new Comment();
-        c4._name = "@losL2r2";
-        c4._text = "Helped me with my issues";
-        _comments.Add(c5);
+        c5._name = "@losL2r2";
+        c5._text = "Helped me with my issues";
+        _biggerCommentList.Add(c5);
 
         Comment c6 = new Comment();
-        c4._name = "@muhsinkiki4245";
-        c4._text = "Nice";
-        _comments.Add(c6);
+        c6._name = "@muhsinkiki4245";
+        c6._text = "Nice";
+        _biggerCommentList.Add(c6);
 
         // VIDEO 3
         Video video3 = new Video();
-        video3._title = "Because of Jesus, You Can Rise Above Anything - Easter 2023 | “Rise”";
+        video3._title = "\nBecause of Jesus, You Can Rise Above Anything - Easter 2023 | “Rise”";
         video3._author = "Church of Jesus Christ | Come Unto Christ";
         video3._length = "102 seconds \n";
         _videoList.Add(video3);
 
         // 3 COMMENTS
+        List<Comment> _comments3 = new List<Comment>();
         Comment c7 = new Comment();
         c7._name = "@peggygallagher@5802";
         c7._text = "Thank you Lord Jesus for picking me up every time I fall...";
+        _biggerCommentList.Add(c7);
 
         Comment c8 = new Comment();
         c8._name = "@trudyappleby1622";
         c8._text = "This is so beautiful and touching thank you. From the broken pieces in me.";
+        _biggerCommentList.Add(c8);
 
         Comment c9 = new Comment();
         c9._name = "@RomonaF1102";
         c9._text = "This was so moving, thank you for creating such uplifting content that gives such powerful reminders of how Christ can help me.";
-
-        foreach (Video video in _videoList)
+        _biggerCommentList.Add(c9);
+        int count = 0;
+        foreach (Video video in _videoList) //_comments
         {
             // below returns the video name, author and length of time
             video.ReturnVideo();
-
-            // should return 3 for each video
             Console.Write($"Number of comments:");
-            if (video.CommentNumberLength(_comments) == 3)
-            {
-                Console.WriteLine($"{video.CommentNumberLength(_comments)}");
-            }
             
-            // below should return the set of specific comments for each video
-            foreach (Comment comment in _comments)
-                {
-                    comment.ReturnComment();
-                }
+            count++;
+            if(count==1)
+            {
+                Console.WriteLine(video.CommentNumberLength(_biggerCommentList)/3);
+                Console.WriteLine(_biggerCommentList[0]._name);
+                Console.WriteLine(_biggerCommentList[0]._text);
+                Console.WriteLine(_biggerCommentList[1]._name);
+                Console.WriteLine(_biggerCommentList[1]._text);
+                Console.WriteLine(_biggerCommentList[2]._name);
+                Console.WriteLine(_biggerCommentList[2]._text);
+            }
+            else if (count==2)
+            {
+                Console.WriteLine(video.CommentNumberLength(_biggerCommentList)/3);
+                Console.WriteLine(_biggerCommentList[3]._name);
+                Console.WriteLine(_biggerCommentList[3]._text);
+                Console.WriteLine(_biggerCommentList[4]._name);
+                Console.WriteLine(_biggerCommentList[4]._text);
+                Console.WriteLine(_biggerCommentList[5]._name);
+                Console.WriteLine(_biggerCommentList[5]._text);
+            }
+            else
+            {
+                Console.WriteLine(video.CommentNumberLength(_biggerCommentList)/3);
+                Console.WriteLine(_biggerCommentList[6]._name);
+                Console.WriteLine(_biggerCommentList[6]._text);
+                Console.WriteLine(_biggerCommentList[7]._name);
+                Console.WriteLine(_biggerCommentList[7]._text);
+                Console.WriteLine(_biggerCommentList[8]._name);
+                Console.WriteLine(_biggerCommentList[8]._text);
+            }
+
         }
     }
 }
