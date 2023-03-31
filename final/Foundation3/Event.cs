5f2ae93e757fa32a988db2ffe42fe96fe061e8fe
Jesus Bejar <israelbejarii@gmail.com>
2023-03-31 10:18:50 -0600

    developed all methods in Event classes

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -15,17 +15,19 @@ public class Event
     private string _time ;
     private Address _address ;
 
-
+    // Standard details - Lists the title, description, date, time, and address.
+    // Full details - Lists all of the above, plus type of event and information specific to that event type. For lectures, this includes the speaker name and capacity. For receptions this includes an email for RSVP. For outdoor gatherings, this includes a statement of the weather.
+    // Short description - Lists the type of event, title, and the date.
     public string DisplayStandardDetails()
     {
-        return "";
+        return $"{_title} {_description} {_date} {_time} {_address}";
     }
     public string DisplayFullDetails()
     {
-        return "";
+        return $"{_title} {_description} {_date} {_time} {_address}";
     }
     public string DisplayFewDetails()
     {
-        return "";
+        return $"{_title} {_description} {_date}";
     }
 }
