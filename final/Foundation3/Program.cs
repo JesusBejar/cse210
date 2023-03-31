2c21d9638bbdb50a48de7a48aedd4a39bb37398f
Jesus Bejar <israelbejarii@gmail.com>
2023-03-31 12:57:12 -0600

    wrote some test code

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+
+        Address a1 = new Address("333 E Wonderview Ave", "Colorado", "United States");
+
+        Reception stanleyDance = new Reception("Ballroom dancing", "\nCome learn to dance at the Stanley Hotel!", "\nTuesday, April 4th", "\n6-10pm", a1, "Reception", "stanleyrsvp@gmail.com");
+        stanleyDance.DisplayStandardDetails();
+        stanleyDance.DisplayFullDetails(stanleyDance.eventType());
+        stanleyDance.DisplayFewDetails();
+        
     }
 }
