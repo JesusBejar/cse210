23491ab07d3b440b6ba4a002e3f02a827542e86a
Jesus Bejar <israelbejarii@gmail.com>
2023-03-31 19:37:44 -0600

    finished making calculate cost method

--- final/Foundation2/CustomerOrder.cs
+++ final/Foundation2/CustomerOrder.cs
@@ -4,35 +4,47 @@ public class CustomerOrder
     private Customer _customer ;
     private int _totalOrderPrice ;
     private Address _address ; 
-    public CustomerOrder(List<Product> productList, Customer customer, int totalOrderPrice, Address address)
+    public CustomerOrder(List<Product> productList, Customer customer, Address address)
     {
         _productList = productList;
         _customer = customer;
-        _totalOrderPrice = totalOrderPrice;
         _address = address;
     }
-    public int CalculateCost(List<Product> productList)
+    public int CalculateCost(bool trueOrFalse)
     {
-        // _totalOrderPrice = 0;
+        _totalOrderPrice = 0;
+        // if (bool varibale) checks to see if the bool variable is true or false
+        // use keywords/variable names not true or false
+        if (trueOrFalse)
+        {
+            _totalOrderPrice += 5;
+        }
+        else if (!trueOrFalse)
+        {
+            // why is this code "unreachable"?
+            _totalOrderPrice += 35;
+        }
+
         foreach (Product p in _productList)
         {
+            // Console.WriteLine(p.TotalProductPrice());
             _totalOrderPrice += p.TotalProductPrice();
+            // Console.WriteLine(_totalOrderPrice);
         }
-        Console.WriteLine($" Total cost - ${_totalOrderPrice}");
+        Console.WriteLine($"Total cost - ${_totalOrderPrice}");
         return _totalOrderPrice;
     }
     public void PackingLabel()
     {
-        Console.WriteLine("Packing label:");
+        Console.WriteLine("Packing label----------");
         foreach (Product p in _productList)
         {
             Console.WriteLine($"{p.ReturnProduct()} ");
         }
-        _productList.Clear();
     }
     public void ShippingLabel()
     {
-        Console.WriteLine($"Shipping label:\n{_customer.ReturnCustomer()}");
+        Console.WriteLine($"Shipping label----------\n{_customer.ReturnCustomer()}");
     }
 
 }
