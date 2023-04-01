37841d47e6a599dbad837dec47593d3abef88a30
Jesus Bejar <israelbejarii@gmail.com>
2023-04-01 10:20:43 -0600

    modified methods and added eventInfo attribute

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -1,6 +1,6 @@
 public class Event
 {
-    public Event(string title, string description, string date, string time, Address address, string eventType)
+    public Event(string title, string description, string date, string time, Address address, string eventType, string eventInfo)
     {
         _title = title;
         _description = description;
@@ -8,6 +8,7 @@ public class Event
         _time = time;
         _address = address;
         _eventType = eventType;
+        _eventInfo = eventInfo;
     }
 
     private string _title ;
@@ -16,6 +17,7 @@ public class Event
     private string _time ;
     private Address _address ;
     private string _eventType ; 
+    private string _eventInfo ;
 
     // Standard details - Lists the title, description, date, time, and address.
     // Full details - Lists all of the above, plus type of event and information specific to that event type. For lectures, this includes the speaker name and capacity. For receptions this includes an email for RSVP. For outdoor gatherings, this includes a statement of the weather.
@@ -25,14 +27,14 @@ public class Event
         Console.WriteLine("\nStandard Details:");
         Console.WriteLine($"{_title} {_description} {_date} {_time} {_address.ReturnAddress()}");
     }
-    public void DisplayFullDetails(string eventType)
+    public void DisplayFullDetails(string eventType, string eventInfo)
     {
         Console.WriteLine("\nFull Details:");
-        Console.WriteLine($"Event type : {_eventType} \n{_title} {_description} {_date} {_time} {_address.ReturnAddress()}");
+        Console.WriteLine($"Event type : {_eventType} \n{_title} {_description} {_date} {_time} {_address.ReturnAddress()} \nAdditional info : {_eventInfo}");
     }
     public void DisplayFewDetails(string eventType)
     {
         Console.WriteLine("\nFew Details:");
-        Console.WriteLine($"{_eventType} {_title} {_date}");
+        Console.WriteLine($"Event type : {_eventType} \n{_title} {_date}");
     }
 }
