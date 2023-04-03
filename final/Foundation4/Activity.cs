5fbc7a86f2cda99404d479fe3831d7cb5fac95c2
Jesus Bejar <israelbejarii@gmail.com>
2023-04-03 10:06:41 -0600

    added GetSummary method and comments

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,18 +1,23 @@
 public abstract class Activity
 {
-    public Activity (string date, string length, int counter, List<Activity> activityList)
+    public Activity (string date, string length, int counter)
     {
         _date = date;
         _length = length;
         _counter = counter;
-        _activityList = activityList;
     }
-    private string _date ; 
-    private string _length ;
-    private int _counter ;
-    private List<Activity> _activityList ;
-
-    public abstract string DistanceCalculation();
+    protected string _date ; 
+    protected string _length ;
+    protected int _counter ;
+    public abstract int DistanceCalculation();
     public abstract string SpeedCalculation();
     public abstract string PaceCalculation();
+
+    // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
+    // make an abstract method for activity type
+    public string GetSummary(int distance, string speed, string pace)
+    {
+        Console.WriteLine($"Date: {_date} ({_length} min) - Distance: {distance.ToString()}km Speed:{speed}kph Pace:{pace}min per km");
+        return "";
+    }
 }
