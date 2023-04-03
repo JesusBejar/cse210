b0a8a033be8bb8963fe963141ed1323131bcce27
Jesus Bejar <israelbejarii@gmail.com>
2023-04-03 10:06:56 -0600

    added test code for running activity

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Running tenMinuteRun = new Running()
+        List<Activity> activityList = new List<Activity>();
+
+        Running tenMinuteRun = new Running("Monday, April 3rd", "10", 1, 5);
+        tenMinuteRun.DistanceCalculation();
+        tenMinuteRun.SpeedCalculation();
+        tenMinuteRun.PaceCalculation();
+
+        tenMinuteRun.GetSummary(
+            tenMinuteRun.DistanceCalculation(),
+            tenMinuteRun.SpeedCalculation(),
+            tenMinuteRun.PaceCalculation()
+        );
+
+        
     }
 }
