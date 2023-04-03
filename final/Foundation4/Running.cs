75e9ea61c3dec6ed009c956cfefac34241ae1253
Jesus Bejar <israelbejarii@gmail.com>
2023-04-03 10:07:48 -0600

    developed distance,speed & pace methods

--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -1,24 +1,26 @@
 public class Running : Activity
 {
-    private string _distance ;
-    public Running (string date, string length, int counter, string distance) : base (date, length, counter)
+    private int _distance ;
+    public Running (string date, string length, int counter, int distance) : base (date, length, counter)
     {
         _distance = distance;
     }
-    public override string DistanceCalculation()
+    public override int DistanceCalculation()
     {
-        return $"{_distance} km";
+        return _distance;
     }
+    // (distance / minutes) * 60
     public override string SpeedCalculation()
     {
-        return "";
+        // 60 / pace
+        return (((_distance / Int32.Parse(_length)) * 60).ToString());
     }
-    public override string PaceCalculation(string length, string distance)
+    public override string PaceCalculation()
     {
         // passes in length and distance
         // converts strings to ints
         // makes the calculation
         // converts ints to strings and returns
-        return ((Int32.Parse(length) * Int32.Parse(distance)).ToString());
+        return ((Int32.Parse(_length) * _distance).ToString());
     }
 }
