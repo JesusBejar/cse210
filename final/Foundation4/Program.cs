4d95b4de973fe22ce012be015a9aedf2f886f62e
Jesus Bejar <israelbejarii@gmail.com>
2023-04-03 12:32:53 -0600

    added test code for biking and swimming classes

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,19 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Activity> activityList = new List<Activity>();
+        List<Activity> _activityList = new List<Activity>();
 
-        Running tenMinuteRun = new Running("Monday, April 3rd", "10", 1, 5);
+        Running tenMinuteRun = new Running("Monday, April 3rd", 10, 1, 5);
         tenMinuteRun.DistanceCalculation();
         tenMinuteRun.SpeedCalculation();
         tenMinuteRun.PaceCalculation();
+        _activityList.Add(tenMinuteRun);
 
-        tenMinuteRun.GetSummary(
-            tenMinuteRun.DistanceCalculation(),
-            tenMinuteRun.SpeedCalculation(),
-            tenMinuteRun.PaceCalculation()
-        );
+        Biking hourLongRide = new Biking("Monday, April 4th", 60, 1, 15);
+        hourLongRide.DistanceCalculation();
+        hourLongRide.SpeedCalculation();
+        hourLongRide.PaceCalculation();
+        _activityList.Add(hourLongRide);
 
-        
+        Swimming thirtyMinutes = new Swimming("Tuesday, April 5ht", 30, 1, 10);
+        thirtyMinutes.DistanceCalculation();
+        thirtyMinutes.SpeedCalculation();
+        thirtyMinutes.PaceCalculation();
+        _activityList.Add(thirtyMinutes);
+
+        foreach (Activity activity in _activityList)
+        {
+            activity.GetSummary(activity.DistanceCalculation(), activity.SpeedCalculation(), activity.PaceCalculation());
+        }
     }
 }
